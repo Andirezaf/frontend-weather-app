250366dd30d065d9a9b3adee81e8000eb3510261
=elangyudha280 <elangyudharakasiwi2@gmail.com>
2024-11-09 12:31:13 -0800

    setup css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React,{useEffect} from "react"
 
 function App() {
+
+  // set title web
+  useEffect(() => {
+    document.title = 'Home'
+  }, [])
+  
+
   return (
-    <section className="relative w-full h-[100vh] bg-slate-200 overflow-x-hidden overflow-y-auto">
+    <section className="relative w-full h-[100vh]  overflow-x-hidden overflow-y-auto p-5 bg-[#ade8f4] flex justify-center items-center">
+      {/*//! container */}
+      <section className="max-w-[800px] border-2 border-red-500">
+
+      </section>
     </section>
   )
 }
